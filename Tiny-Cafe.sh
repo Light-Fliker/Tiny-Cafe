314e94aa5060c18fe93d16c5c5bc402b4f8330c8
Light-Fliker <mohabelkabir4@gmail.com>
2025-05-13 17:48:52 -0400

    menu

--- Tiny-Cafe.sh
+++ Tiny-Cafe.sh
@@ -8,10 +8,27 @@ for ((i=0; i<${#text}; i++)); do
     sleep 0.1
 done
 echo " "
-text="    Hello sir"
-for ((i=0; i<${#text}; i++)); do
-    echo -n "${text:$i:1}"
+text1="    Hello sir"
+for ((i=0; i<${#text1}; i++)); do
+    echo -n "${text1:$i:1}"
     sleep 0.1
 done
 echo " "
 for i in {1..20}; do echo -n "-"; sleep 0.05; done
+echo ""
+menu="🌿  Here is the menu  🌿"
+for ((i=0; i<${#menu}; i++)); do
+    echo -n "${menu:$i:1}"
+    sleep 0.1
+done
+echo ""
+for i in {1..20}; do echo -n "*"; sleep 0.05; done
+echo -e "☕ Coffee    ......... \$3.00"
+echo -e "🍵 Tea       ......... \$2.50"
+echo -e "🥐 Croissant ......... \$4.00"
+echo -e "🍰 Cake      ......... \$5.50\n"
+for i in {1..20}; do echo -n "*"; sleep 0.05; done
+echo -e "what would you like to eat?\n"
+read choice
+echo ""
+echo "Excellent choice...enjoy your ${choice}!!"
