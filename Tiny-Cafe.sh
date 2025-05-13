e1d6b7bf6bd99343790f498b2286d026a2fb4972
Light-Fliker <mohabelkabir4@gmail.com>
2025-05-13 17:31:02 -0400

    design

--- Tiny-Cafe.sh
+++ Tiny-Cafe.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 clear
-for i in {1..10}; do echo -n "-"; sleep 0.05; done
-echo -e "\nthing"
-for i in {1..10}; do echo -n "-"; sleep 0.05; done
+for i in {1..20}; do echo -n "-"; sleep 0.05; done
+echo " "
+text="Welcome to my Cafe"
+for ((i=0; i<${#text}; i++)); do
+    echo -n "${text:$i:1}"
+    sleep 0.1
+done
+echo " "
+for i in {1..20}; do echo -n "-"; sleep 0.05; done
