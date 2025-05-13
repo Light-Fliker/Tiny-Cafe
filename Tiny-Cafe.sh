93c78001c059225576866362f9f665269aa89e80
Light-Fliker <mohabelkabir4@gmail.com>
2025-05-13 17:35:25 -0400

    title

--- Tiny-Cafe.sh
+++ Tiny-Cafe.sh
@@ -8,4 +8,10 @@ for ((i=0; i<${#text}; i++)); do
     sleep 0.1
 done
 echo " "
+text="    Hello sir"
+for ((i=0; i<${#text}; i++)); do
+    echo -n "${text:$i:1}"
+    sleep 0.1
+done
+echo " "
 for i in {1..20}; do echo -n "-"; sleep 0.05; done
